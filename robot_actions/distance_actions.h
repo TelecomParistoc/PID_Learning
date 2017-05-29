95f15c09357c5b6d0e4cb872ddbd15c066c17578
superpingu <arnaudbonetti@me.com>
2017-05-30 00:29:28 +0200

    debugging in progess

--- robot_actions/distance_actions.h
+++ robot_actions/distance_actions.h
@@ -6,6 +6,9 @@
 
 #include <stdint.h>
 
+#define X_ORIGIN 1000
+#define Y_ORIGIN 1000
+
 
 void reset_pid_distance(uint32_t p, uint32_t i, uint32_t d);
 bool move_and_measure_distance(double& delay, double& integrated_differential, const PID& return_pid);
