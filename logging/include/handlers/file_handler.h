2b2867fb822f36335fed0198086ec83da592db15
Alkanor <Alkanoor@github.com>
2018-02-01 23:04:22 +0100

    shared instead of unique

--- logging/include/handlers/file_handler.h
+++ logging/include/handlers/file_handler.h
@@ -16,9 +16,9 @@ class File_handler : public Ostream_handler<File_handler<Filename, trunc_if_exis
         static bool initialize()
         {
             if(trunc_if_exists)
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::trunc)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::trunc)));
             else
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::app)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::app)));
 
             if(!_private_ostream)
                 throw std::runtime_error("Error: Unable to open \""+std::string(Filename)+"\" for logging. (you can try to create directory to solve the issue)");
@@ -28,11 +28,11 @@ class File_handler : public Ostream_handler<File_handler<Filename, trunc_if_exis
         }
 
     private:
-        static std::shared_ptr<std::ofstream> _private_ostream;
+        static std::unique_ptr<std::ofstream> _private_ostream;
 };
 
 template <const char* Filename, bool trunc_if_exists>
-std::shared_ptr<std::ofstream> File_handler<Filename, trunc_if_exists>::_private_ostream;
+std::unique_ptr<std::ofstream> File_handler<Filename, trunc_if_exists>::_private_ostream;
 
 
 template <const char* Filename, typename Behaviour_factory, typename Input_type_Factory, Input_type_Factory Bad_File_Behaviour, bool trunc_if_exists = false>
@@ -44,9 +44,9 @@ class File_handler_fail_behaviour : public Ostream_handler<File_handler_fail_beh
         static bool initialize()
         {
             if(trunc_if_exists)
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::trunc)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::trunc)));
             else
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::app)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename, std::ios::out | std::ios::app)));
 
             if(!_private_ostream)
             {
@@ -61,11 +61,11 @@ class File_handler_fail_behaviour : public Ostream_handler<File_handler_fail_beh
         }
 
     private:
-        static std::shared_ptr<std::ofstream> _private_ostream;
+        static std::unique_ptr<std::ofstream> _private_ostream;
 };
 
 template <const char* Filename, typename Behaviour_factory, typename Input_type_Factory, Input_type_Factory Bad_File_Behaviour, bool trunc_if_exists>
-std::shared_ptr<std::ofstream> File_handler_fail_behaviour<Filename, Behaviour_factory, Input_type_Factory, Bad_File_Behaviour, trunc_if_exists>::_private_ostream;
+std::unique_ptr<std::ofstream> File_handler_fail_behaviour<Filename, Behaviour_factory, Input_type_Factory, Bad_File_Behaviour, trunc_if_exists>::_private_ostream;
 
 
 template <typename Filename_aggregator, bool trunc_if_exists = false>
@@ -77,9 +77,9 @@ class Dynamic_header_file_handler : public Ostream_handler<Dynamic_header_file_h
         static bool initialize()
         {
             if(trunc_if_exists)
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate(""), std::ios::out | std::ios::trunc)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate("").c_str(), std::ios::out | std::ios::trunc)));
             else
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate(""), std::ios::out | std::ios::app)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate("").c_str(), std::ios::out | std::ios::app)));
 
             if(!_private_ostream)
                 throw std::runtime_error("Error: Unable to open \""+Filename_aggregator::aggregate("")+"\" for logging. (you can try to create directory to solve the issue)");
@@ -89,11 +89,11 @@ class Dynamic_header_file_handler : public Ostream_handler<Dynamic_header_file_h
         }
 
     private:
-        static std::shared_ptr<std::ofstream> _private_ostream;
+        static std::unique_ptr<std::ofstream> _private_ostream;
 };
 
 template <typename Filename_aggregator, bool trunc_if_exists>
-std::shared_ptr<std::ofstream> Dynamic_header_file_handler<Filename_aggregator, trunc_if_exists>::_private_ostream;
+std::unique_ptr<std::ofstream> Dynamic_header_file_handler<Filename_aggregator, trunc_if_exists>::_private_ostream;
 
 
 template <typename Filename_aggregator, typename Behaviour_factory, typename Input_type_Factory, Input_type_Factory Bad_File_Behaviour, bool trunc_if_exists = false>
@@ -105,9 +105,9 @@ class Dynamic_header_file_handler_fail_behaviour : public Ostream_handler<Dynami
         static bool initialize()
         {
             if(trunc_if_exists)
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate(""), std::ios::out | std::ios::trunc)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate("").c_str(), std::ios::out | std::ios::trunc)));
             else
-                _private_ostream = std::move(std::make_shared<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate(""), std::ios::out | std::ios::app)));
+                _private_ostream = std::move(std::make_unique<std::ofstream>(new std::ofstream(Filename_aggregator::aggregate("").c_str(), std::ios::out | std::ios::app)));
 
             if(!_private_ostream)
             {
@@ -122,11 +122,11 @@ class Dynamic_header_file_handler_fail_behaviour : public Ostream_handler<Dynami
         }
 
     private:
-        static std::shared_ptr<std::ofstream> _private_ostream;
+        static std::unique_ptr<std::ofstream> _private_ostream;
 };
 
 template <typename Filename_aggregator, typename Behaviour_factory, typename Input_type_Factory, Input_type_Factory Bad_File_Behaviour, bool trunc_if_exists>
-std::shared_ptr<std::ofstream> Dynamic_header_file_handler_fail_behaviour<Filename_aggregator, Behaviour_factory, Input_type_Factory, Bad_File_Behaviour, trunc_if_exists>::_private_ostream;
+std::unique_ptr<std::ofstream> Dynamic_header_file_handler_fail_behaviour<Filename_aggregator, Behaviour_factory, Input_type_Factory, Bad_File_Behaviour, trunc_if_exists>::_private_ostream;
 
 
 #endif
